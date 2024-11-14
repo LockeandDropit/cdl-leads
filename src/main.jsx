3795a8ea401a77ed49bacd65a967b363b2727609
Sven Olafsen <stockburner9@gmail.com>
2024-11-13 21:24:37 -0600

    added api key

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+posthog.init(import.meta.env.POSTHOG_API, {
+  api_host: "https://us.i.posthog.com",
+});
+
+createRoot(document.getElementById("root")).render(
+  <PostHogProvider>
     <App />
-  </StrictMode>,
-)
+  </PostHogProvider>
+);
